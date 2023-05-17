f64379c187f087269ffe7fc88a12143771f59809
Olga Boiko <Itnumber1@ukr.net>
2023-05-17 23:58:08 +0300

    add task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const refs={start:document.querySelector('[data-start]'),
+stop:document.querySelector('[data-stop]'),
+body:document.querySelector('body')
+}
+let timerId = null;
+// refs.stop.disabled = true;
+// refs.start.disabled = true;
+function startBtnDisable(){
+   
+}
+refs.start.addEventListener('click',()=>{timerId = setInterval(addBodyColor, 1000)} );
 
+function addBodyColor(){
+    refs.body.style.backgroundColor = getRandomHexColor(),
+    refs.start.setAttribute('disabled', true),
+    refs.stop.removeAttribute('disabled')
+}
+refs.stop.addEventListener("click", () => {
+        clearInterval(timerId),
+        refs.start.removeAttribute('disabled'),
+        refs.stop.setAttribute('disabled', true)
+});
+
+function getRandomHexColor() {
+        return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
