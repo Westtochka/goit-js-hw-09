4c637b08ea591796c8cd8819569b670245f7710a
Olga Boiko <Itnumber1@ukr.net>
2023-05-18 00:30:28 +0300

    start task2

---
+++ src/js/02-timer.js
@@ -0,0 +1,10 @@
+startBtn=document.querySelector('#data-start');
+inputDate=document.querySelector('#datetime-picker');
+const targetDate=new Date();
+
+// function replaceAttrBtn(){
+// inputDate.removeAttribute(type)
+// inputDate.setAttribute(type, date)  
+// };
+
+startBtn.addEventListener('click',()=>{})
