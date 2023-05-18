ce65fcfebe4115a9ad59a1e4f78659281b90c61e
Westtochka <119757679+Westtochka@users.noreply.github.com>
2023-05-18 08:06:27 +0000

    Deploying to gh-pages from @ Westtochka/goit-js-hw-09@dd6d804de91b41dcaf2a2accbe127f1bd4dd255a 🚀

--- 02-timer.246a35d7.js
+++
@@ -1,2 +0,0 @@
-startBtn=document.querySelector("#data-start"),inputDate=document.querySelector("#datetime-picker");const t=new Date("Jan 1, 2024 00:00:00");console.log(t);Data.now;
-//# sourceMappingURL=02-timer.246a35d7.js.map
