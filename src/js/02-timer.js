dd6d804de91b41dcaf2a2accbe127f1bd4dd255a
Olga Boiko <Itnumber1@ukr.net>
2023-05-18 11:05:58 +0300

    task2 add const inMs

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,27 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
 startBtn=document.querySelector('#data-start');
 inputDate=document.querySelector('#datetime-picker');
 const targetDate=new Date('Jan 1, 2024 00:00:00');
-console.log(targetDate)
-const currentDataMs=Data.now;
+
+setInterval(()=>{
+const currentDataMs=Date.now();
+const targetDateinMs=targetDate.getTime();
+
+const distanceToTargetDate=targetDateinMs - currentDataMs;
+
+const oneSecondInMs = 1000;
+const oneMinuteInMs = oneSecondInMs * 60;
+const oneHourInMs = oneMinuteInMs * 60;
+const oneDayInMs = oneHourInMs * 24;
+const daysRemaining = Math.floor(distanceToTargetDate / oneDayInMs);
+const hoursRemaining= Math.floor((distanceToTargetDate % oneDayInMs)/oneHourInMs); 
+const minutesRemaining=Math.floor((distanceToTargetDate % oneHourInMs)/oneMinuteInMs)
+const secondsRemaining=Math.floor((distanceToTargetDate % oneMinuteInMs)/oneSecondInMs)
+console.log(daysRemaining)
+console.log(hoursRemaining)
+console.log(minutesRemaining)
+console.log(secondsRemaining)
+}, 1000)
+
