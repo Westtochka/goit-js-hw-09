db26d9e098cc68bab07feab50dcb1de0fb43d651
Olga Boiko <Itnumber1@ukr.net>
2023-05-18 10:12:07 +0300

    remove input type

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,10 +1,5 @@
 startBtn=document.querySelector('#data-start');
 inputDate=document.querySelector('#datetime-picker');
-const targetDate=new Date();
-
-// function replaceAttrBtn(){
-// inputDate.removeAttribute(type)
-// inputDate.setAttribute(type, date)  
-// };
-
-startBtn.addEventListener('click',()=>{})
+const targetDate=new Date('Jan 1, 2024 00:00:00');
+console.log(targetDate)
+const currentDataMs=Data.now;
