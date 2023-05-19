2cba2a242b1334a488e0de1ac2fd3ff0d7f07271
Olga Boiko <Itnumber1@ukr.net>
2023-05-19 21:12:02 +0300

    task2 add new code(timer works)

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,10 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+let timerId;//нужен для очистки интервала
+let inputCurrentDate=null;// будет равен дате, которую вібрали в инпуте
+let differTime = null;//разница между вводом и настоящей датой
 
 const startBtn=document.querySelector('[data-start]');
 const inputDate=document.querySelector('#datetime-picker');
@@ -8,11 +13,10 @@ const hoursTimer=document.querySelector('[data-hours]')
 const minutesTimer=document.querySelector('[data-minutes]')
 const secondsTimer=document.querySelector('[data-seconds]')
 
-const oneSecondInMs = 1000;
-const oneMinuteInMs = oneSecondInMs * 60;
-const oneHourInMs = oneMinuteInMs * 60;
-const oneDayInMs = oneHourInMs * 24;
+startBtn.disabled = true;
 
+// const targetDate=new Date (inputDate.value)  //дата в инпуте
+// console.log(inputDate.value)
 
 const options = {
   enableTime: true,
@@ -20,51 +24,67 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    // if selectedDates.getUTCMilliseconds()>
-    console.log(selectedDates[0].value);
-const targetDate=new Date(selectedDates);
-
-  },
-};
-// startBtn.setAttribute('disabled', true)   //кнопка неактивна
-
+    const currentDataMs=Date.now()
+    if (selectedDates[0]-currentDataMs<0){//дата выбранная через библиотеку[0]-стартовая дата-это дата сейчас
+      startBtn.disabled=true,
+      Notify.failure('Please choose a date in the future');
+    }
+    else {startBtn.disabled=false  
+      inputCurrentDate=selectedDates[0];//дату, которую выбрали в инпуте=дате, которую возвращает библиотекаб т.е. эти данные отображаются в инпуте
+  }
+  }}
 
 flatpickr(inputDate, options)
 
-new Date (inputDate.value)
-console.log(inputDate.value)
+startBtn.addEventListener('click',onStartBtn)
 
+function onStartBtn(e){//эта функция каждую секунду сравнивает 2 даты-введенную и сейчас
+  startBtn.disabled = true;
+  inputDate.disabled = true;
+// кнопка и инпут неактивні
+//считаем разницу между датой, выбранной в инпуте и сегодня. 
+  timerId=setInterval(()=>{
+const currentData=Date.now()//дата, которая сейчас
+const differTime = inputCurrentDate - currentData;//разница между выбранной пользователем датой и между датой сейчас  
 
-startBtn.addEventListener('click',makeTimer)
-function makeTimer(){
-setInterval(()=>{
-const currentDataMs=Date.now();
-const targetDateinMs=targetDate.getTime();
+createInterfaceTimer(convertMs(differTime))// передаем данные для замещения их в тексконтент и отображении на экране
+}, 1000);
+}
+//эта функция или блокирует или передает данные на экран в текстконтент
+function createInterfaceTimer({daysRemaining,hoursRemaining, minutesRemaining, secondsRemaining}){
+  if(differTime < 0){//если разница с минусом, то время в прошлом и удаляем по ид сет интервалб делаем кнопки и поле инпута неактивными
+    clearInterval(timerId);
+    startBtn.disabled = false;
+    inputDate.disabled = false;
+      return
+  }
+  else{
+    daysTimer.textContent=daysRemaining
+    hoursTimer.textContent=hoursRemaining
+    minutesTimer.textContent=minutesRemaining
+    secondsTimer.textContent=secondsRemaining
+  }
+}
 
-const distanceToTargetDate=targetDateinMs - currentDataMs;
+function addLeadingZero(value){
+  return String(value).padStart(2, '0');
+}
 
-// const oneSecondInMs = 1000;
-// const oneMinuteInMs = oneSecondInMs * 60;
-// const oneHourInMs = oneMinuteInMs * 60;
-// const oneDayInMs = oneHourInMs * 24;
-const daysRemaining = Math.floor(distanceToTargetDate / oneDayInMs);
-const hoursRemaining= Math.floor((distanceToTargetDate % oneDayInMs)/oneHourInMs); 
-const minutesRemaining=Math.floor((distanceToTargetDate % oneHourInMs)/oneMinuteInMs)
-const secondsRemaining=Math.floor((distanceToTargetDate % oneMinuteInMs)/oneSecondInMs)
-daysTimer.textContent=daysRemaining
-hoursTimer.textContent=hoursRemaining
-minutesTimer.textContent=minutesRemaining
-secondsTimer.textContent=secondsRemaining
-}, 1000)};
+//эта функция конвертирует полученные данные из инпута в милисекундах -в дни, часы, минуты, секунды и выодит объект с этими данными
+// конвертор, переводим данные в мс => в дни, часы,
+//  минуты, секунды
 
+function convertMs(ms){
+  const oneSecondInMs = 1000;
+  const oneMinuteInMs = oneSecondInMs * 60;
+  const oneHourInMs = oneMinuteInMs * 60;
+  const oneDayInMs = oneHourInMs * 24;
 
+const daysRemaining = addLeadingZero(Math.floor(ms / oneDayInMs));
+const hoursRemaining= addLeadingZero(Math.floor((ms % oneDayInMs)/oneHourInMs)); 
+const minutesRemaining=addLeadingZero(Math.floor((ms % oneHourInMs)/oneMinuteInMs));
+const secondsRemaining=addLeadingZero(Math.floor((ms % oneMinuteInMs)/oneSecondInMs));
 
+return {daysRemaining,hoursRemaining, minutesRemaining, secondsRemaining}
+  };
 
-
-
-
-
-//1.Додати бібліотеку дата в инпуті
-//2.При виборі дати перевіряти чи не знаходиться вона в минулому
-//3.Повісити слухача на кнопку клік і зробити її одразу неактивною і
-//  на інпут при вводі перевіряти дату, щоб була більше за дату, яка заразб 
