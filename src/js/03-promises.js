3c486755709a3cf80f85b2e6b699ca3b700313c7
Olga Boiko <Itnumber1@ukr.net>
2023-05-20 19:08:37 +0300

    task3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,42 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
+
+import { Notify } from 'notiflix';
+const form=document.querySelector('form')
+const amountInput=document.querySelector('[amount]')
+const delayInput =document.querySelector('[delay]')
+const stepInput=document.querySelector('[step]')
+const btnCreate=document.querySelector('button')
+
+// console.log(amount)
+form.addEventListener('submit',submitForm)
+let position =0;
+const amount = Number(amountInput.value);
+const delay = Number(delayInput.value);
+const step = Number(stepInput.value);
+
+function createPromise(position, delay){
+const promise= new Promise((resolve, reject)=> {
+  setTimeout(() => {
+    const shouldResolve = Math.random() > 0.3;
+    if (shouldResolve) {
+      resolve({ position, delay })
   } else {
-    // Reject
+    reject({ position, delay })
   }
+  }, delay);
+});}
+
+function submitForm (evt){
+  evt.preventDefault();
+  createPromise()
+
 }
+// function createPromise(2, 1500)
+//   .then(({ position, delay }) => {
+//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   })
+//   .catch(({ position, delay }) => {
+//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//   });
+
+
+
