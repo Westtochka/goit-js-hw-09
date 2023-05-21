f64d591b46ca2ef2fff6969fc0b3896915b66764
Olga Boiko <Itnumber1@ukr.net>
2023-05-21 14:00:27 +0300

    task3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,20 +1,19 @@
 
 import { Notify } from 'notiflix';
+
 const form=document.querySelector('form')
 const amountInput=document.querySelector('[amount]')
 const delayInput =document.querySelector('[delay]')
 const stepInput=document.querySelector('[step]')
 const btnCreate=document.querySelector('button')
 
-// console.log(amount)
+
 form.addEventListener('submit',submitForm)
 let position =0;
-const amount = Number(amountInput.value);
-const delay = Number(delayInput.value);
-const step = Number(stepInput.value);
+
 
 function createPromise(position, delay){
-const promise= new Promise((resolve, reject)=> {
+return new Promise((resolve, reject)=> {
   setTimeout(() => {
     const shouldResolve = Math.random() > 0.3;
     if (shouldResolve) {
@@ -27,8 +26,16 @@ const promise= new Promise((resolve, reject)=> {
 
 function submitForm (evt){
   evt.preventDefault();
+// const amount = Number(amountInput.value);
+// const delay = Number(delayInput.value);
+// const step = Number(stepInput.value);
   createPromise()
-
+.then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
 }
 // function createPromise(2, 1500)
 //   .then(({ position, delay }) => {
